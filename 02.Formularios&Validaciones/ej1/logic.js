e8d8d413fc69cbff7fa007fa2b1f44ff1d1f651e
Andrea Vieira Hernández <alum.dvieirah.2021@iesalixar.org>
2022-10-14 13:18:49 +0200

    Formularios y validaciones
    Ejercicios con expresiones regulares y handlers

--- 02.Formularios&Validaciones/ej1/logic.js
+++ 02.Formularios&Validaciones/ej1/logic.js
@@ -11,88 +11,103 @@ NOTA: puedes utilizar tanto validaciones HTML como lo aprendido en Javascript.
 En caso de que se informe correctamente guardar la información en un objeto y obtener el
 JSON correspondiente, en caso contrario mostrar un mensaje de error.
 */
-function registerUser() {
-  const $FORM = document.getElementById("Ej1");
-  const $DNI = document.getElementById("dni");
-  const $NAME = document.getElementById("nombre");
-  const $SURNAME = document.getElementById("apellidos");
-  const $PHONE = document.getElementById("telefono");
-  const $COMMENT = document.getElementById("comentario");
-  const $HOUR = document.getElementById("hora");
+const $FORM = document.getElementById("Ej1");
+const $DNI = document.getElementById("dni");
+const $NAME = document.getElementById("nombre");
+const $SURNAME = document.getElementById("apellidos");
+const $PHONE = document.getElementById("telefono");
+const $COMMENT = document.getElementById("comentario");
+const $HOUR = document.getElementById("hora");
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    const dni = $DNI.value;
-    const name = $NAME.value;
-    const surname = $SURNAME.value;
-    const phone = $PHONE.value;
-    const comment = $COMMENT.value;
-    const hour = $HOUR.value;
+function handleSubmit(e) {
+  e.preventDefault();
+  const dni = $DNI.value;
+  const name = $NAME.value;
+  const surname = $SURNAME.value;
+  const phone = $PHONE.value;
+  const comment = $COMMENT.value;
+  const hour = $HOUR.value;
 
-    if (dni == false || isValidDni(dni) == false) {
+  if (dni == false || isValidDni(dni) == false) {
+    alert(
+      "El dni debe ser un valor válido : \n -Debe estar relleno \n -Debe seguir este modelo: 00.000.000-A \n -La letra debe corresponder"
+    );
+  } else {
+    if (name == false || isValidText(name) == false) {
       alert(
-        "El dni debe ser un valor válido : \n -Debe estar relleno \n -Debe seguir este modelo: 00.000.000-A \n -La letra debe corresponder"
+        "El nombre debe ser un valor válido : \n -Debe estar relleno \n -Debe estar compuesto por 1 o 2 palabras"
       );
     } else {
-      if (name == false || isValidText(name) == false) {
+      if (surname == false || isValidText(surname) == false) {
         alert(
-          "El nombre debe ser un valor válido : \n -Debe estar relleno \n -Debe estar compuesto por 1 o 2 palabras"
+          "El o los apellidos debe ser un valor válido : \n -Debe estar relleno \n -Debe estar compuesto por 1 o 2 palabras"
         );
       } else {
-        if (surname == false || isValidText(surname) == false) {
+        if (phone == false || isValidPhone(phone) == false) {
           alert(
-            "El o los apellidos debe ser un valor válido : \n -Debe estar relleno \n -Debe estar compuesto por 1 o 2 palabras"
+            "El teléfono debe ser un valor válido : \n -Debe estar relleno \n -Debe estar compuesto por un prefijo (+00) y 9 números"
           );
         } else {
-          if (phone == false || isValidPhone(phone) == false) {
+          if (comment == false || isValidComent(comment) == false) {
             alert(
-              "El teléfono debe ser un valor válido : \n -Debe estar relleno \n -Debe estar compuesto por un prefijo (+00) y 9 números"
+              "El comentario debe ser un valor válido : \n -Debe estar relleno \n -Debe tener un máximo de 250 caracteres"
             );
           } else {
-            if (comment == false || isValidComent(comment) == false) {
-              alert(
-                "El comentario debe ser un valor válido : \n -Debe estar relleno \n -Debe tener un máximo de 250 caracteres"
-              );
+            if (hour == false || isValidHour(hour) == false) {
+              alert("La hora debe ser un valor válido :  \n -Debe estar relleno \n -Debe corresponder al formato 24h");
             } else {
-              if (hour == false || isValidHour(hour) == false) {
-                alert("La hora debe ser un valor válido :  \n -Debe estar relleno \n -Debe corresponder al formato 24h");
-              } else {
-                alert("Datos presentados con éxito");
-              }
+              alert("Datos presentados con éxito");
+              let regUser = {
+                dni: dni,
+                name: name,
+                surname: surname,
+                phone: phone,
+                comment: comment,
+                hout: hour
+              };
+              console.log(regUser);
+              let newUser = JSON.stringify(regUser);
+              console.log(newUser);
+              document.getElementById("newUser").innerHTML = newUser;
             }
           }
         }
       }
     }
   }
-  function isValidDni(dni) {
-    const validacion = /^\d{2}\.\d{3}\.\d{3}-[A-Z]$/;
-    if (validacion.test(dni)) {
-      numbers = dni.substring(0, dni.length - 2);
-      onlyNumbers = numbers.replaceAll(".", "");
-      userLetter = dni[dni.length - 1];
-      position = parseInt(onlyNumbers) % 23;
-      letters = "TRWAGMYFPDXBNJZSQVHLCKET";
-      letter = letters.substring(position, position + 1);
-    }
-    return (letter == userLetter.toUpperCase())
-  }
-  function isValidText(text) {
-    const validacion =
-      /^(([A-Za-záéíóúÁÉÍÓÚ]+)|([A-Za-záéíóúÁÉÍÓÚ]+\s[A-Za-záéíóúÁÉÍÓÚ]+))$/;
-    return validacion.test(text);
-  }
-  function isValidPhone(phone) {
-    const validacion = /^(\+\d{2}\s\d{9})$/;
-    return validacion.test(phone);
-  }
-  function isValidComent(comment) {
-    const validacion = /^[\s\S]{1,250}$/;
-    return validacion.test(comment);
-  }
-  function isValidHour(hour) {
-    const validacion = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
-    return validacion.test(hour);
-  }
-  $FORM.addEventListener("submit", handleSubmit);
 }
+
+function isValidDni(dni) {
+  const validacion = /^\d{2}\.\d{3}\.\d{3}-[A-Z]$/;
+  const letters = "TRWAGMYFPDXBNJZSQVHLCKET";
+  let letter = -1, userLetter, position, numbers;
+  if (validacion.test(dni)) {
+    numbers = (dni.substring(0, dni.length - 2)).replaceAll(".", "");
+    userLetter = (dni[dni.length - 1]).toUpperCase();
+    position = parseInt(numbers) % 23;
+    letter = letters.substring(position, position + 1);
+  }
+  return (letter === userLetter)
+}
+
+function isValidText(text) {
+  const validacion =
+  /^(([A-Za-záéíóúÁÉÍÓÚ]{2,})|([A-Za-záéíóúÁÉÍÓÚ]{2,}\s[A-Za-záéíóúÁÉÍÓÚ]{2,}))$/;
+  return validacion.test(text);
+}
+
+function isValidPhone(phone) {
+  const validacion = /^(\+\d{2}\s\d{9})$/;
+  return validacion.test(phone);
+}
+
+function isValidComent(comment) {
+  const validacion = /^[\s\S]{1,250}$/;
+  return validacion.test(comment);
+}
+
+function isValidHour(hour) {
+  const validacion = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
+  return validacion.test(hour);
+}
+$FORM.addEventListener("submit", handleSubmit);
---
+++ 02.Formularios&Validaciones/ej3/logic.js
@@ -0,0 +1,119 @@
+/*
+Valida el formulario con los siguientes campos y en caso de que sean correctos los datos
+introducidos se almacenen en un objeto y en caso contrario muestre un mensaje de error.
+ - DNI (99.999.999-X)
+ - Nombre (Mínimo 1 nombre y 1 apellido, Máximo 2 nombre y 2 apellidos)
+ - Fecha de Nacimiento (dd/mm/yyyy)
+ - Email (xxxxxx@yyyyy.zzz)
+ - WEB (http://www.xxxx.yyy)
+ - Contraseña (Entre 8 y 10 carácteres)
+Ningún campo podrá dejarse en blanco.
+NOTA: puedes utilizar tanto validaciones HTML como lo aprendido en Javascript.
+En caso de que se informe correctamente guardar la información en un objeto y obtener el
+JSON correspondiente, en caso contrario mostrar un mensaje de error.
+*/
+const $FORM = document.getElementById("Ej3");
+const $DNI = document.getElementById("dni");
+const $NAME = document.getElementById("name");
+const $BIRTH = document.getElementById("birth");
+const $EMAIL = document.getElementById("email");
+const $WEB = document.getElementById("web");
+const $PASSWORD = document.getElementById("password");
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const dni = $DNI.value;
+  const name = $NAME.value;
+  const birth = $BIRTH.value;
+  const email = $EMAIL.value;
+  const web = $WEB.value;
+  const password = $PASSWORD.value;
+
+  if (dni == false || isValidDni(dni) == false) {
+    alert(
+      "El dni debe ser un valor válido : \n -Debe estar relleno \n -Debe seguir este modelo: 00.000.000-A \n -La letra debe corresponder"
+    );
+  } else {
+    if (name == false || isValidText(name) == false) {
+      alert(
+        "El nombre debe ser un valor válido : \n -Debe estar relleno \n -Debe estar compuesto por 1 o 2 palabras"
+      );
+    } else {
+      if (birth == false || isValidBirth(birth) == false) {
+        alert(
+          "La fecha de nacimiento debe ser un valor válido : \n -Debe estar relleno \n -Debe seguir este formato: dd/mm/aaaa"
+        );
+      } else {
+        if (email == false || isValidEmail(email) == false) {
+          alert(
+            "El email debe ser un valor válido : \n -Debe estar relleno \n -Debe seguir este formato: xxxxxx@yyyyy.zzz)"
+          );
+        } else {
+          if (web == false || isValidWeb(web) == false) {
+            alert(
+              "La web personal debe ser un valor válido : \n -Debe estar relleno \n -Debe tener este formato: http://www.xxxx.yyy"
+            );
+          } else {
+            if (password == false || isValidPassword(password) == false) {
+              alert(
+                "La contraseña debe ser un valor válido : \n -Debe estar relleno \n -Debe tener entre 8 y 10 caracteres"
+              );
+            } else{
+              alert("Datos presentados con éxito");
+              let regUser = {
+                dni: dni,
+                name: name,
+                birth: birth.split('-').reverse().join('/'),
+                email: email,
+                web: web,
+                password: password
+              };
+              console.log(regUser);
+              let newUser = JSON.stringify(regUser);
+              console.log(newUser);
+              document.getElementById("newUser").innerHTML = newUser;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+function isValidDni(dni) {
+  const validacion = /^\d{2}\.\d{3}\.\d{3}-[A-Z]$/;
+  const letters = "TRWAGMYFPDXBNJZSQVHLCKET";
+  let letter = -1, userLetter, position, numbers;
+  if (validacion.test(dni)) {
+    numbers = (dni.substring(0, dni.length - 2)).replaceAll(".", "");
+    userLetter = (dni[dni.length - 1]).toUpperCase();
+    position = parseInt(numbers) % 23;
+    letter = letters.substring(position, position + 1);
+  }
+  return (letter === userLetter)
+}
+function isValidText(text) {
+  const validacion =
+    /^(([A-Za-záéíóúÁÉÍÓÚ]{2,})|([A-Za-záéíóúÁÉÍÓÚ]{2,}\s[A-Za-záéíóúÁÉÍÓÚ]{2,}))$/;
+  return validacion.test(text);
+}
+function isValidBirth(birth) {
+  let currDate = new Date();
+  const newOrder = birth.split('-').reverse().join('/');
+  const validacion = /^([0-2][0-9]|3[0-1])(\/|-)(0[1-9]|1[0-2])\2(\d{4})$/;
+  if(newOrder<currDate.toLocaleDateString()){
+    return validacion.test(newOrder);
+  } else{return false}
+}
+function isValidEmail(email) {
+  const validacion = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]{2,}@[a-zA-Z0-9-]{2,}(?:\.[a-zA-Z0-9-]+)*$/;
+  return validacion.test(email);
+}
+function isValidWeb(web) {
+  const validacion = /^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/;
+  return validacion.test(web);
+}
+function isValidPassword(password) {
+  const validacion = /^\S{8,10}$/;
+  return validacion.test(password);
+}
+$FORM.addEventListener("submit", handleSubmit);
